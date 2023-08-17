58f5099970094ff87fa750f9ddc7113a0aafe572
ANUSHIA <anuga10@gmail.com>
2023-08-17 03:47:04 -0700

    subtract commit

--- src/calcpack/calc.java
+++ src/calcpack/calc.java
@@ -7,13 +7,21 @@ public class calc {
 		return c;
 
 	}
+	
+	public int sub(int a, int b) {
+		int c= a-b;
+		return c;
+
+	}
 
 	public static void main(String[] args) {
 		int i=5;
 		int j=10;
 		calc obj = new calc();
 		int Result = obj.add(i,j);
+		int Result1 = obj.sub(j,i);
 		System.out.println(Result);
+		System.out.println(Result1);
 	}
 
 }
